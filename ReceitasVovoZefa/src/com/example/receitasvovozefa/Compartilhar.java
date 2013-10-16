87fb3f09f84599a29ff4ddce5d46727829f6e073
fcas <f3l1nux@gmail.com>
2013-10-16 16:01:49 -0300

    Interação entre os layouts

--- ReceitasVovoZefa/src/com/example/receitasvovozefa/Compartilhar.java
+++ ReceitasVovoZefa/src/com/example/receitasvovozefa/Compartilhar.java
@@ -1,7 +1,8 @@
 package com.example.receitasvovozefa;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 
 public class Compartilhar extends Activity {
@@ -17,6 +18,14 @@ public class Compartilhar extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.compartilhar, menu);
 		return true;
+	}
+	
+	@Override
+	public void onBackPressed() {
+		Intent voltaIntent = new Intent();
+		voltaIntent.setClass(Compartilhar.this, MainActivity.class);
+		startActivity(voltaIntent);
+		finish();
 	}
 
 }

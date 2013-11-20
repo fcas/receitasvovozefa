e2625c280cad327179779cc002acbf6b1590f1a7
fcas <f3l1nux@gmail.com>
2013-11-19 23:03:20 -0300

    Mudando a estrutura do projeto

---
+++ src/activity/ListaReceitas.java
@@ -0,0 +1,33 @@
+package activity;
+
+import com.example.receitasvovozefa.*;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+
+
+public class ListaReceitas extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_lista_receitas);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.lista_receitas, menu);
+		return true;
+	}
+	
+	@Override
+	public void onBackPressed() {
+		Intent voltaIntent = new Intent();
+		voltaIntent.setClass(ListaReceitas.this, MainActivity.class);
+		startActivity(voltaIntent);
+		finish();
+	}
+
+}
---
+++ src/activity/MinhasReceitas.java
@@ -0,0 +1,23 @@
+package activity;
+
+import com.example.receitasvovozefa.*;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MinhasReceitas extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_minhas_receitas);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.minhas_receitas, menu);
+		return true;
+	}
+
+}

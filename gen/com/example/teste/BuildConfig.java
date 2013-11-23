20e219a8c06e3677491612b65112e2d5fefc71e6
fcas <f3l1nux@gmail.com>
2013-11-23 12:18:10 -0300

    pasta test

--- gen/com/example/teste/BuildConfig.java
+++ gen/com/example/teste/BuildConfig.java
@@ -1,5 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
 package com.example.teste;
 
 public final class BuildConfig {
-  public static final boolean DEBUG = true;
+    public final static boolean DEBUG = true;
 }
